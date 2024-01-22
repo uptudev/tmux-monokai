c5795ec5a6d98605171896c9d808603e74414494
uptu <uptu@uptu.dev>
2024-01-22 00:20:32 -0700

    test10

--- scripts/monokai.sh
+++ scripts/monokai.sh
@@ -217,12 +217,12 @@ main()
 
   # Window option
   if $show_powerline; then
-    tmux set-window-option -g window-status-current-format "#[bg=${white},fg=${gray}]${left_sep} #[fg=${black},bg=${white}]#I #W${current_flags} #[bg=default,fg=${white}]${left_sep}"
+    tmux set-window-option -g window-status-current-format "#[bg=${white},fg=${gray}]${left_sep} #[fg=${black},bg=${white}]#I #W${current_flags} #[bg=${white},fg=${white}]${left_sep}"
   else
     tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${white}] #I #W${current_flags} "
   fi
 
-  tmux set-window-option -g window-status-format "#[bg=default,fg=default]${left_sep} #[fg=${white},bg=default]#I #W${flags}"
+  tmux set-window-option -g window-status-format "#[bg=default,fg=default]${left_sep} #[fg=${white},bg=default]#I #W${flags} #[bg=default,fg=default]${left_sep}"
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
   tmux set-window-option -g window-status-separator ""
