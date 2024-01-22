a82ebfdaee5524f5f6ee4f892ec5f117768a61c6
uptu <uptu@uptu.dev>
2024-01-21 19:08:35 -0700

    window status transparent

--- scripts/monokai.sh
+++ scripts/monokai.sh
@@ -222,7 +222,7 @@ main()
     tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${white}] #I #W${current_flags} "
   fi
 
-  tmux set-window-option -g window-status-format "#[bg=default,fg=${gray}]${left_sep} #[fg=${white},bg=${gray}]#I #W${flags} #[bg=${gray},fg=${gray}]${left_sep}"
+  tmux set-window-option -g window-status-format "#[bg=default,fg=${gray}]${left_sep} #[fg=${white},bg=default]#I #W${flags} #[bg=default,fg=${gray}]${left_sep}"
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
   tmux set-window-option -g window-status-separator ""
