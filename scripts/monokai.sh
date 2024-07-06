4455a3446ee417f62524f1b7766ad795993f4610
uptu <uptu@uptu.dev>
2024-07-05 22:15:33 -0600

    Why wont this turn transparent?

--- scripts/monokai.sh
+++ scripts/monokai.sh
@@ -128,7 +128,7 @@ main()
 
   # Status left
   if $show_powerline; then
-    tmux set-option -g status-left "#[fg=${green}]#{?client_prefix,#[fg=${yellow}],}#[bg=${green},fg=${black},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
+    tmux set-option -g status-left "#[bg=${green}]#{?client_prefix,#[fg=${yellow}],}#[bg=${green},fg=${black},bold]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},bg=#{default}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
     powerbg=default
   else
     tmux set-option -g status-left "#[bg=${green},fg=${black},bold]#{?client_prefix,#[bg=${magenta}],} ${left_icon} "
